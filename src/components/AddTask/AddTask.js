05bceaf55afe9d1311f8e3f8453d0068980f1046
mirajulalam <alifmirajulalam@gmail.com>
2022-10-29 20:48:05 -0700

    react day picker calender added

--- src/components/AddTask/AddTask.js
+++ src/components/AddTask/AddTask.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import swal from 'sweetalert';
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
 
 const AddTask = () => {
+  const [date,setDate]=useState(new Date())
 
     const handleSubmit =event=>{
         event.preventDefault();
@@ -28,10 +31,16 @@ const AddTask = () => {
     }
 
     return (
-          <div className="hero min-h-screen bg-base-200">
-   <div className="hero-content text-center">
-    <div className="max-w-md">
-    <div className="card-body">
+       <section className="max-w-7xl mx-auto px-12">
+  <div className="flex justify-center items-center my-16 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
+    <div className="flex justify-center ">
+         <DayPicker 
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+         />
+    </div>
+    <div className="flex justify-center">
     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
       <div className="card-body">
         <h1 className='text-secondary text-2xl font-bold'>Task Name</h1>
@@ -55,9 +64,9 @@ const AddTask = () => {
       </div>
     </div>
     </div>
-    </div>
-    </div>
   </div>
+</section>
+         
     );
 };
 
