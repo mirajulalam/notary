0da4c28eb97ca7243eb6ec48248cc6565545781a
mirajulalam <alifmirajulalam@gmail.com>
2022-10-29 18:58:52 -0700

    footer components added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 import Tasklist from './components/Tasklist/Tasklist';
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' element={<Home/>}/>
         <Route path='/tasklist' element={<Tasklist/>}/>
       </Routes>
+      <Footer/>
     </div>
   );
 }
